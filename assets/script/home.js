f9e984b7c24332bb2886873bd85fe48f10689cdc
Lateefat Obisesan <temilele06@gmail.com>
2026-05-06 14:58:41 -0500

    adding storage

--- assets/script/home.js
+++ assets/script/home.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const postButton = document.getElementById('button');
+const textArea = document.getElementById('text');
+const fileInput = document.getElementById('file');
+const fileNameDisplay = document.getElementById('file-name');
+const feed = document.getElementById('feed');
+const randomUsersContainer = document.getElementById('random-users');
+const logoutBtn= document.getElementById('logout');
+
+logoutBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.removeItem('isLoggedIn');
+    window.location.href = 'index.html';
+})
