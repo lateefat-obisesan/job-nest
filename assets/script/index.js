e5dc6b7a56fa4607540dfb32032b011909d98383
jaspreet <jasspreet4640@gmail.com>
2026-05-08 04:15:04 -0500

    add forget password

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,26 +4,16 @@ localStorage.setItem("username", "ade");
 localStorage.setItem("password", "1234");
 
 'use strict';
-
 localStorage.setItem('username', 'admin');
 localStorage.setItem('password', '1234');
-
 const loginForm = document.querySelector('#login-form');
-
 const username = document.querySelector('#username');
-
 const password = document.querySelector('#password');
-
 const loginError = document.querySelector('#login-error');
-
 loginForm.addEventListener('submit', function(event) {
-
     event.preventDefault();
-
     const savedUsername = localStorage.getItem('username');
-
     const savedPassword = localStorage.getItem('password');
-
     if (
         username.value === savedUsername &&
         password.value === savedPassword
